ff648e9ac3c8c016d5b46549e3801ec0ecc66129
cabanga <manucabanga@gmail.com>
2022-02-23 15:32:40 +0100

    this commit config hash function get

--- src/routes/file/file.routes.js
+++ src/routes/file/file.routes.js
@@ -4,13 +4,13 @@ import { upload_document } from '#controllers/file.controller'
 import multer from 'multer'
 
 //const upload = multer({ dest: 'public/' }).single("document");
-upload = multer({ dest: 'public/' })
+const upload = multer({ dest: 'public/' })
 
 
 
 const router = Router()
 
-router.route('/upload_document', upload.single('document')).post(upload_document)
+router.route('/upload_document').post(upload.single('document'), upload_document)
 
 
 
